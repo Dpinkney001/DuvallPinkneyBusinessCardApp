d0a5b140b45362426501e80ec5f0b60110318f31
Dpinkney001 <duvall.pinkny@gmail.com>
2023-08-14 01:51:13 -0400

    first draft. needs formatting

--- app/src/main/java/com/example/duvallpinkneybusinesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/duvallpinkneybusinesscardapp/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.duvallpinkneybusinesscardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.duvallpinkneybusinesscardapp.ui.theme.DuvallPinkneyBusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingImage(
+                        stringResource(R.string.full_name_text),
+                        stringResource(R.string.title_text),
+                        stringResource(R.string.phone_text),
+                        stringResource(R.string.website_text),
+                        stringResource(R.string.email_text),
+                        stringResource(R.string.linkedin_text)
+                    )
                 }
             }
         }
@@ -30,17 +49,103 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun GreetingText(name: String, website: String,phone: String, title: String, email: String, linkedin: String, modifier: Modifier = Modifier) {
+    // Create a column so that texts don't overlap
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = name,
+            fontSize = 36.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Text(
+            text = title,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .padding(end = 16.dp)
+                .align(alignment = Alignment.End)
+        )
+
+        // Create a column so that texts don't overlap
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier
+        ) {
+            Text(
+                text = website,
+                fontSize = 36.sp,
+                lineHeight = 116.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Text(
+                text = email,
+                fontSize = 36.sp,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .padding(end = 16.dp)
+                    .align(alignment = Alignment.End)
+            )
+            Text(
+                text = linkedin,
+                fontSize = 36.sp,
+                lineHeight = 116.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Text(
+                text = phone,
+                fontSize = 36.sp,
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .padding(end = 16.dp)
+                    .align(alignment = Alignment.End)
+            )
+        }
+    }
 }
+
+@Composable
+fun GreetingImage(name: String, website: String,phone: String, title: String, email: String, linkedin: String, modifier: Modifier = Modifier) {
+    // Create a box to overlap image and texts
+    Box(modifier) {
+        Image(
+            painter = painterResource(id = R.drawable.android_logo),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            name = name,
+            title = title,
+            website = website,
+            email = email,
+            linkedin = linkedin,
+            phone = phone,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DuvallPinkneyBusinessCardAppPreview() {
     DuvallPinkneyBusinessCardAppTheme {
-        Greeting("Android")
+        GreetingImage(
+            stringResource(R.string.full_name_text),
+            stringResource(R.string.title_text),
+            stringResource(R.string.phone_text),
+            stringResource(R.string.website_text),
+            stringResource(R.string.email_text),
+            stringResource(R.string.linkedin_text)
+        )
     }
 }
